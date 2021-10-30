60aaf727d7632f7fd0529316dde301dad36d1a32
Anant <anant312004@gmail.com>
2021-10-30 12:12:19 +0530

    Created new project music player

---
+++ Music_player.py
@@ -0,0 +1,20 @@
+import random
+import os
+import time
+
+while True:
+    c = random.randint(1, 2)
+    if c == 1:
+        music_folder = "D:\\d data\\NCS music"
+        songs = os.listdir(music_folder)
+        a = random.randint(1, len(songs) - 1)
+        os.startfile(os.path.join(music_folder, songs[a]))
+        time.sleep(60*5)
+    elif c == 2:
+        music_folder = "D:\\d data\\New songs"
+        songs = os.listdir(music_folder)
+        a = random.randint(1, len(songs) - 1)
+        os.startfile(os.path.join(music_folder, songs[a]))
+        time.sleep(60*5)
+    else:
+        pass
