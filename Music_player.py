fb5637c8a9997fbd5c3c542ec43d4b19e494c537
Anant <anant312004@gmail.com>
2021-10-31 15:48:55 +0530

    Did something

--- Music_player.py
+++ Music_player.py
@@ -10,13 +10,13 @@ while True:
         songs = os.listdir(music_folder)
         a = random.randint(1, len(songs) - 1)
         os.startfile(os.path.join(music_folder, songs[a]))
-        time.sleep(60*5)
+        time.sleep(60*4)
     elif c == 2:
         music_folder = "D:\\d data\\New songs"
         songs = os.listdir(music_folder)
         a = random.randint(1, len(songs) - 1)
         os.startfile(os.path.join(music_folder, songs[a]))
-        time.sleep(60*5)
+        time.sleep(60*4)
     else:
         pass
 
